c0d63f5ba7d43b841ef99233639912093f794e63
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-07-07 14:40:27 +0300

    first

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ Cart.prototype.addItem = function(product, quantity) {
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
   let data = JSON.stringify(this.items);
-    localStorage.setItem('cartitem', data);
+  localStorage.setItem('cartitem', data);
 };
 
 Cart.prototype.removeItem = function(item) {
